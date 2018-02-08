8c84a4d79076c2186655f411cefefd9e82c1eb62
Axel Ekwall <axel@minimize.se>
2018-02-08 22:17:15 +0100

    Added Axios and started working on Twitch request

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,44 @@
-const functions = require('firebase-functions');
+// Create and Deploy Your First Cloud Functions
+// https://firebase.google.com/docs/functions/write-firebase-functions
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+// Require stuff
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const axios = require('axios');
+
+// Init the Firebase Admin SDK
+admin.initializeApp(functions.config().firebase);
+
+// Init Axios Twitch
+const twitch = axios.create({
+  baseURL: 'https://api.twitch.tv/helix',
+});
+const twitchAuth = axios.create({
+  baseURL: 'https://api.twitch.tv/kraken',
+});
+
+// TODO: Function to get App Token
+const getTwitchToken = (clientKey, clientSecret) => {
+  return appAccessToken;
+};
+
+// Function to get data from Twitch API
+exports.getTwitchData = functions.https.onRequest((req, res) => {
+  const accessToken = getTwitchToken(clientKey, clientSecret);
+
+  twitch
+    .get({
+      url: '/streams',
+      params: {
+        first: 50,
+        type: 'live',
+      },
+    })
+    .then((response) => {
+      // TODO: Save to Firestore
+      return res.send(200, response);
+    })
+    .catch((error) => {
+      res.send(500, 'Server error');
+    });
+});
