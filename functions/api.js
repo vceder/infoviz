0db223d0cd664d67da1b1194c15f28f537a8412a
Axel Ekwall <axel@minimize.se>
2018-02-22 11:55:59 +0100

    remove comments

--- functions/api.js
+++ functions/api.js
@@ -46,39 +46,7 @@ router.get('/users', (req, res) => {
     console.log('Här kommer top 100!');
     // SKicka alla top 100 users
   }
-
-  /*
-	var docRef = db.collection("cities").doc("SF");
-
-	docRef.get().then(function(doc) {
-	    if (doc.exists) {
-	        console.log("Document data:", doc.data());
-	    } else {
-	        // doc.data() will be undefined in this case
-	        console.log("No such document!");
-	    }
-	}).catch(function(error) {
-	    console.log("Error getting document:", error);
-	});*/
 });
-
-// // catch 404 and forward to error handler
-// app.use((req, res, next) => {
-//   var err = new Error('Not Found');
-//   err.status = 404;
-//   next(err);
-// });
-
-// // error handler
-// app.use((err, req, res, next) => {
-//   // set locals, only providing error in development
-//   res.locals.message = err.message;
-//   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-//   // render the error page
-//   res.status(err.status || 500);
-//   res.render('error');
-// });
 
 app.use('/api', router);
 
