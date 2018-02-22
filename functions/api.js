a518a3e84f37765d209786ee92b45d5a6549fa48
Axel Ekwall <axel@minimize.se>
2018-02-22 17:38:41 +0100

    Fix for function

--- functions/api.js
+++ functions/api.js
@@ -30,40 +30,37 @@ router.get('/users', (req, res) => {
   if (requestID) {
     // skicka specifik user
     const usersRef = db.collection('users').doc(requestID);
-    usersRef.get().then((doc) => {
-      if (doc.exists) {
-        return res.status(200).send(doc.data());
-      } else {
-        // doc.data() will be undefined in this case
-        console.log('No such document!');
-        return res.status(500);
-      }
-    })
-    .catch((error) => {
-      res.status(500).send(error);
-    });
-  }
-
-  else {
-    const usersRef = db.collection('users')
-    var obj_counter = 0;
-    var top_hundred = [];
-    usersRef.get().then(function(querySnapshot){
-      querySnapshot.forEach(function(doc){
-        obj_counter++;
-        if (obj_counter >= 101){
-          return false;
+    usersRef
+      .get()
+      .then((doc) => {
+        if (doc.exists) {
+          return res.status(200).send(doc.data());
+        } else {
+          // doc.data() will be undefined in this case
+          console.log('No such document!');
+          return res.status(500);
         }
-        top_hundred.push(doc.data());
-        console.log(obj_counter, " => ", doc.data());
+      })
+      .catch((error) => {
+        res.status(500).send(error);
       });
-      return res.status(200).send(top_hundred);
-    }
-  )
-  .catch((error) => {
-    res.status(500).send(error);
-  });
-}
+  } else {
+    const usersRef = db.collection('users');
+    let top_hundred = [];
+    usersRef
+      .orderBy('last_live_timestamp')
+      .limit(100)
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          top_hundred.push(doc.data());
+        });
+        return res.status(200).send(top_hundred);
+      })
+      .catch((error) => {
+        res.status(500).send(error);
+      });
+  }
 });
 
 app.use('/api', router);
