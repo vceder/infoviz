8d860a712fe4090853858af16cbd5cfae2565036
Viktor Ceder <vceder@kth.se>
2018-03-03 15:52:18 +0100

    Staging comit, trying new slider

--- client/src/components/Slider.vue
+++ client/src/components/Slider.vue
@@ -1,20 +1,101 @@
 <template>
-  <div class="slider" id="n">
-      HELLO WORLD!
-  </div>
+    <div class="">
+        <svg @mousemove="mousover()" :width="width" :height="height">
+            <g :style="{transform: 'translate(${margin.left}px, ${margin.top}px)'}">
+                <!-- <path class="" :d=""/> template for path -->
+                <path id="axis" class="line" :d="paths.line" :style="{transform: 'translate(${width})'}" />
+                <path id="slideSelector" class="selector" :d="paths.selector" />
+            </g>
+        </svg>
+    </div>
 </template>
 
 <script>
+// @ is an alias to /src
+import * as d3 from 'd3';
+
+// const data = [0000, 0030, 0100, 0130, 0200, 0230, 0300]; // Dummy data, later to be replaced with data from firebase with our current time intervalls
+
+const props = {
+    data: {
+        type: Array,
+        default: () => ["2018-03-01 12:00:00", "2018-03-01 12:30:00"],
+    },
+    margin: {
+        type: Object,
+        default: () => ({
+            top: 50,
+            right: 50,
+            bottom: 0,
+            left: 50
+        })
+    },
+    ceil: {
+        type: Number,
+        default: 100,
+    }
+};
+
 export default {
-  name: 'Slider',
-  data() {
-    return {
+    name: 'Slider',
+    data() {
+        return {
+            width: 0,
+            height: 0,
+            paths: {
+                line: '',
+                selector: '',
+            },
+            scaled: {
+                x: null,
+                y: null,
+            },
+            data: ["2018-03-01 12:00:00", "2018-03-01 12:30:00"],
+        };
+    },
+    computed: {
+        padded() {
+            const width = this.width - this.margin.left - this.margin.right;
+            const height = this.height - this.margin.top - this.margin.bottom;
+            return {width, height}
+        }
+    },
+    mounted() {
+        // window.addEventListener('resize', this.onResize);
+        // this.onResize();
+        this.update();
 
-    };
-  },
-  mounted() {
+    },
+    beforeDestroy(){
+        // window.removeEventListener('resize', this.onResize);
+    },
+    watch: {
+        data: function dataChanged(newData, oldData) {
+            //Function for updateing slider, if this is nessecery
+        },
+        width: function widthChanged() {
+            // this.initialaize();
+            this.update();
+        },
+    },
+    methods: {
+        update() {
+            const line = d3.line();
+            // console.log(this.data[this.data.length -1])
+            let x = d3.scaleTime()
+                .domain([this.data[0], this.data[this.data.length - 1]])
+                .range([0, this.width])
+                .clamp(true);
 
-  },
+
+
+
+        },
+        mouseover(){
+            console.log("EVENT!")
+        }
+    },
+
 };
 </script>
 
@@ -22,195 +103,3 @@ export default {
 <style scoped lang="scss">
 
 </style>
-
-<!-- <body>
-  <script>
-    var formatDateIntoYear = d3.timeFormat("%Y");
-    // var formatDate = d3.timeFormat("%b %Y");
-    var formatDate = d3.timeFormat("%a %d")
-    var parseDate = d3.timeParse("%m/%d/%y");
-
-    // Datumformat: February 20, 2018 at 9:31:02 PM UTC+1
-
-    // var startDate = new Date("2004-11-01"),
-    //   endDate = new Date("2017-04-01");
-
-    var startDate = new Date("2018-02-16"),
-      endDate = new Date("2018-02-23");
-
-
-    var margin = {
-        top: 50,
-        right: 50,
-        bottom: 0,
-        left: 50
-      },
-      width = 960 - margin.left - margin.right,
-      height = 500 - margin.top - margin.bottom;
-
-    var svg = d3.select("#vis")
-      .append("svg")
-      .attr("width", width + margin.left + margin.right)
-      .attr("height", height + margin.top + margin.bottom);
-
-    ////////// slider //////////
-
-    var moving = false;
-    var currentValue = 0;
-    var targetValue = width;
-
-    var playButton = d3.select("#play-button");
-
-    var x = d3.scaleTime()
-      .domain([startDate, endDate])
-      .range([0, targetValue])
-      .clamp(true);
-
-    var slider = svg.append("g")
-      .attr("class", "slider")
-      .attr("transform", "translate(" + margin.left + "," + height / 5 + ")");
-
-    slider.append("line")
-      .attr("class", "track")
-      .attr("x1", x.range()[0])
-      .attr("x2", x.range()[1])
-      .select(function() {
-        return this.parentNode.appendChild(this.cloneNode(true));
-      })
-      .attr("class", "track-inset")
-      .select(function() {
-        return this.parentNode.appendChild(this.cloneNode(true));
-      })
-      .attr("class", "track-overlay")
-      .call(d3.drag()
-        .on("start.interrupt", function() {
-          slider.interrupt();
-        })
-        .on("start drag", function() {
-          currentValue = d3.event.x;
-          console.log(currentValue)
-          update(x.invert(currentValue));
-        })
-      );
-
-    slider.insert("g", ".track-overlay")
-      .attr("class", "ticks")
-      .attr("transform", "translate(0," + 18 + ")")
-      .selectAll("text")
-      .data(x.ticks(9))
-      .enter()
-      .append("text")
-      .attr("x", x)
-      .attr("y", 20)
-      .attr("text-anchor", "middle")
-      .text(function(d) {
-        return formatDate(d);
-      });
-
-    var handle = slider.insert("circle", ".track-overlay")
-      .attr("class", "handle")
-      .attr("r", 9);
-
-    var label = slider.append("text")
-      .attr("class", "label")
-      .attr("text-anchor", "middle")
-      .text(formatDate(startDate))
-      .attr("transform", "translate(0," + (-25) + ")")
-
-
-    ////////// plot //////////
-
-    var dataset;
-
-    var plot = svg.append("g")
-      .attr("class", "plot")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
-    var file = "feb.csv"
-    d3.csv(file, prepare, function(data) {
-      dataset = data;
-      console.log(dataset)
-      drawPlot(dataset);
-
-      playButton
-        .on("click", function() {
-          var button = d3.select(this);
-          if (button.text() == "Pause") {
-            moving = false;
-            clearInterval(timer);
-            // timer = 0;
-            button.text("Play");
-          } else {
-            moving = true;
-            timer = setInterval(step, 100);
-            button.text("Pause");
-          }
-          console.log("Slider moving: " + moving);
-        })
-    })
-
-    function prepare(d) {
-      d.id = d.id;
-      d.date = parseDate(d.date);
-      return d;
-    }
-
-    function step() {
-      update(x.invert(currentValue));
-      currentValue = currentValue + (targetValue / 151);
-      if (currentValue > targetValue) {
-        moving = false;
-        currentValue = 0;
-        clearInterval(timer);
-        // timer = 0;
-        playButton.text("Play");
-        console.log("Slider moving: " + moving);
-      }
-    }
-
-    function drawPlot(data) {
-      var locations = plot.selectAll(".location")
-        .data(data);
-
-      // if filtered dataset has more circles than already existing, transition new ones in
-      // locations.enter()
-      //   .append("circle")
-      //   .attr("class", "location")
-      //   .attr("cx", function(d) {
-      //     return x(d.date);
-      //   })
-      //   .attr("cy", height / 2)
-      //   .style("fill", function(d) {
-      //     return d3.hsl(d.date / 1000000000, 0.8, 0.8)
-      //   })
-      //   .style("stroke", function(d) {
-      //     return d3.hsl(d.date / 1000000000, 0.7, 0.7)
-      //   })
-      //   .style("opacity", 0.5)
-      //   .attr("r", 8)
-      //   .transition()
-      //   .duration(400)
-      //   .attr("r", 25)
-      //   .transition()
-      //   .attr("r", 8);
-
-      // if filtered dataset has less circles than already existing, remove excess
-      locations.exit()
-        .remove();
-    }
-
-    function update(h) {
-      // update position and text of label according to slider scale
-      handle.attr("cx", x(h));
-      label
-        .attr("x", x(h))
-        .text(formatDate(h));
-
-      // filter data set and redraw plot
-      var newData = dataset.filter(function(d) {
-        return d.date < h;
-      })
-      drawPlot(newData);
-    }
-  </script>
-</body> -->
