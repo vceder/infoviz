cf653ad10cb31b53f0e870adac598dadd8a34e11
Arvid Larsson <arvidlarzzon@hotmail.com>
2018-03-03 16:01:17 +0100

    Included HoverDetails in Home

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <div class="home">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    </div>
+    <HoverDetails/>
+   </div>
 </template>
 
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import HoverDetails from '@/components/HoverDetails.vue';
 
 export default {
   name: 'home',
   components: {
     HelloWorld,
-  },
+    HoverDetails
+  }
 };
 </script>
 
