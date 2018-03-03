3042114922ea2b5a5fa3b97dbec290b2cb1865e0
Arvid Larsson <arvidlarzzon@hotmail.com>
2018-03-03 15:46:03 +0100

    Hover details in games overview
    Data to be formatted after games data is supplied.

--- client/src/components/HoverDetails.vue
+++ client/src/components/HoverDetails.vue
@@ -1,28 +1,50 @@
 <template>
   <div class="overview-details">
-    <h2>{{gameData.name}}</h2>
+    <div class="staticHeadline">Game Name</div>
+    <div class="changingValues">{{games.name}}</div>
+    <div class="changingValues"><img v-bind:src="games.image" alt="<Image undefined>"></div>
+    <div class="staticHeadline">Time of day</div>
     <!--Get timezone of user to display correct timestamp-->
-    <p>Time: {{gameData.timestamp}}</p>
-    <p>Viewers: {{gameData.viewer_count}}</p>
-    <!-- <img :src="gameData.box_art_url.replace('{width}x{height}', '200x300')" alt=""> -->
+    <div class="changingValues">{{games.timestamp}}</div>
+    <div class="staticHeadline">Current viewers</div>
+    <div class="changingValues">{{games.viewer_count}}</div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HoverDetails',
-  data() {
-    return {
-      gameData: '',
-    };
-  },
-  mounted() {
-    
-  },
-};
+  import { mapState } from 'vuex';
+
+  export default {
+    name: 'HoverDetails',
+    computed: {
+      ...mapState(['games']),
+    },
+    mounted() {
+      
+    },
+  };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
+@import './assets/style/global.scss';
 
+  .overview-details{
+      float: right;
+      margin: 10% 5% 0 0;
+  }
+  .staticHeadline{
+      color: white;
+      font-family: Lato;
+      font-weight: 300;
+      font-size: 15px;
+  }
+  .changingValues{
+      color:#E81B5F;
+      font-family: Lato;
+      font-weight: 400;
+      margin-bottom: 5%;
+      font-size: 20px;
+
+  }
 </style>
