067bced544fef79657216fb81f2530b81fb8fc9b
linneaholm <a.linnea.holm@gmail.com>
2018-03-04 16:06:52 +0100

    commitment issues. intro css fix

--- client/src/views/Intro.vue
+++ client/src/views/Intro.vue
@@ -3,10 +3,11 @@
   <div class="intro_page">
     <div id="titleHolder">
       <div id="logo"></div>
-      <div id="intro">plot twitch</div>
       <div id="introText">
         A quick real time overview for the top 100 Twitch streams
 </div>
+      <div id="intro">plot twitch</div>
+
 <div id="launch">
   <router-link to="/overview" id="launch_button">Show me</router-link>
 </div>
