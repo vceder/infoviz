99c5eeaf37a602f34b150534fce4a4fe51bc9b88
linneaholm <a.linnea.holm@gmail.com>
2018-03-04 14:26:04 +0100

    css småfix

--- client/src/views/Intro.vue
+++ client/src/views/Intro.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="intro_page">
-    <h1 id="intro">Plot Twitch</h1>
-    <router-link to="/overview" id="launch_button">Let's get started!</router-link>
+    <div id="titleHolder">
+      <div id="intro">Plot Twitch</div>
+    <div id="launch"><router-link to="/overview" id="launch_button">Let's get started!</router-link>
   </div>
+</div>
+</div>
 </template>
