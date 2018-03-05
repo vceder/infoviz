3c63a887d23884b5b47920374cdd06cfe27c1c8c
tasola <petter.tk@hotmail.com>
2018-03-05 11:35:19 +0100

    colors according to graphic profile

--- client/src/components/ThumbnailPlot.vue
+++ client/src/components/ThumbnailPlot.vue
@@ -50,7 +50,7 @@ export default {
     const opacityCircles = 0.7;
 
     function color(n) {
-      var colors = ["#3366cc", "#dc3912", "#ff9900", "#109618", "#990099", "#0099c6", "#dd4477", "#66aa00", "#b82e2e", "#316395", "#994499", "#22aa99", "#aaaa11", "#6633cc", "#e67300", "#8b0707", "#651067", "#329262", "#5574a6", "#3b3eac"];
+      var colors = ["#3CDCA0", "#F7766F", "#9FFF70", "#9169F8", "#F9C872", "#83C9FD"]
       return colors[n % colors.length];
     }
 
@@ -78,8 +78,7 @@ export default {
       .attr('r', this.radius)
       .style('opacity', opacityCircles)
       .style('fill', d => {
-        // console.log(this.gameId)
-        // return color(this.gameId);
+        // console.log(d.game_id)
         return color(d.game_id);
       });
   },
