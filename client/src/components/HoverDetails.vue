ef978e7f6155a024e3a2b811f4ee64631b1f1a35
Axel Ekwall <axel@minimize.se>
2019-03-08 22:38:59 +0100

    client code style

--- client/src/components/HoverDetails.vue
+++ client/src/components/HoverDetails.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="overview-details">
-    <div class="changingValues"><img :src="gameImg"></div>
     <div class="changingValues">
-      {{gameName}}
+      <img :src="gameImg">
     </div>
+    <div class="changingValues">{{gameName}}</div>
     <div class="static-headline">Current Viewers</div>
-    <div class="changing-values">
-      {{getNumber()}}
-    </div>
+    <div class="changing-values">{{getNumber()}}</div>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex';
-import gameColor from '@/assets/js/colorsMixin.js';
+import { mapState } from "vuex";
+import gameColor from "@/assets/js/colorsMixin.js";
 
 export default {
   mixins: [gameColor],
-  name: 'HoverDetails',
+  name: "HoverDetails",
   data() {
     return {
-      gameImg: '',
-      gameName: '',
-      imgWidth: '150',
-      imgHeight: '200',
+      gameImg: "",
+      gameName: "",
+      imgWidth: "150",
+      imgHeight: "200"
     };
   },
   watch: {
@@ -32,26 +30,26 @@ export default {
     }
   },
   computed: {
-    ...mapState(['current', 'games']),
+    ...mapState(["current", "games"])
   },
-  props: ['gameID'],
+  props: ["gameID"],
   methods: {
     getGame() {
       if (this.games[this.gameID]) {
         this.gameImg = this.games[this.gameID].box_art_url.replace(
-          '{width}x{height}',
-           this.imgWidth + 'x' + this.imgHeight
+          "{width}x{height}",
+          this.imgWidth + "x" + this.imgHeight
         );
         console.log(this.games[this.gameID].box_art_url);
         this.gameName = this.games[this.gameID].name;
         return true;
       } else {
         this.$store
-          .dispatch('getGameInfo', this.gameID)
+          .dispatch("getGameInfo", this.gameID)
           .then(res => {
             this.gameImg = res.box_art_url.replace(
-              '{width}x{height}',
-              this.imgWidth + 'x' + this.imgHeight
+              "{width}x{height}",
+              this.imgWidth + "x" + this.imgHeight
             );
             this.gameName = res.name;
             return true;
@@ -68,46 +66,44 @@ export default {
       if (this.current.games[this.gameID] !== undefined) {
         return this.current.games[this.gameID].totalViewers;
       }
-    },
+    }
   },
   mounted() {
     this.getGame();
-  },
+  }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
-  .game-img{
-    max-height: 30px;
-  }
-  .overview-details{
-    box-sizing: border-box;
-    float: left;
-    width: 100%;
-    display: flex;
-    flex-direction: column;
-  }
-  .static-headline{
-    width: 100%;
-    display: block;
-    color: white;
-    font-family: Lato;
-    font-weight: 700;
-    text-transform: uppercase;
-    letter-spacing: 2px;
-    font-size: 0.8vw;
-  }
-  .changing-values{
-    width: 100%;
-    display: block;
-    color: #E81B5F;
-    font-family: Lato;
-    font-weight: 300;
-    text-transform: uppercase;
-    font-size: 1.3vw;
-    margin-bottom: 4%;
-
-  }
+.game-img {
+  max-height: 30px;
+}
+.overview-details {
+  box-sizing: border-box;
+  float: left;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+}
+.static-headline {
+  width: 100%;
+  display: block;
+  color: white;
+  font-family: Lato;
+  font-weight: 700;
+  text-transform: uppercase;
+  letter-spacing: 2px;
+  font-size: 0.8vw;
+}
+.changing-values {
+  width: 100%;
+  display: block;
+  color: #e81b5f;
+  font-family: Lato;
+  font-weight: 300;
+  text-transform: uppercase;
+  font-size: 1.3vw;
+  margin-bottom: 4%;
+}
 </style>
--- client/src/views/Intro.vue
+++ client/src/views/Intro.vue
@@ -1,21 +1,16 @@
 <template>
-  <html>
+<html>
   <div class="intro_page">
     <div id="titleHolder">
       <div id="logo"></div>
-      <div id="introText">
-        A quick real time overview for the top 100 Twitch streams
-</div>
+      <div id="introText">A quick real time overview for the top 100 Twitch streams</div>
       <div id="title">Plot Twitch</div>
-      <div id="SubintroText">
-        Your data driven universe service
-</div>
+      <div id="SubintroText">Your data driven universe service</div>
 
-<div id="launch">
-  <router-link to="/overview" id="launch_button">Show me</router-link>
-</div>
-</div>
-
-</div>
+      <div id="launch">
+        <router-link to="/overview" id="launch_button">Show me</router-link>
+      </div>
+    </div>
+  </div>
 </html>
 </template>
